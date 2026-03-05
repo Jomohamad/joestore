750e8e901150c2e7dddbd3f4f6a1ae2df7d766f7
Jomohamad <143020755+Jomohamad@users.noreply.github.com>
2026-03-05 14:57:26 +0000

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   next-env.d.ts
    	modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Keep Next build artifacts in `dist` to match current Vercel output directory setting.
+  distDir: 'dist',
 };
 
 module.exports = nextConfig;
